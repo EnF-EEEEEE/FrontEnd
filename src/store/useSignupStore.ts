8cd613029dad4a46e5f1d0b541db5b049f5783f0
Kathleen Kang <kathleenkang@Kathleens-MacBook-Pro.local>
2025-03-25 07:27:04 +0900

    fix: signup user category page

--- src/store/useSignupStore.ts
+++ src/store/useSignupStore.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 
-interface UserCategory {
+export interface UserCategory {
   career: boolean;
   mental: boolean;
   relationship: boolean;
