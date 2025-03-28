01c6b6570a4491f3d5e665358c396383b1ad83c0
Kathleen Kang <kathleenkang@Kathleens-MacBook-Pro.local>
2025-03-29 07:59:36 +0900

    fix: use h-full in common layout

--- src/components/layout/CommonLayout.tsx
+++ src/components/layout/CommonLayout.tsx
@@ -1,25 +1,29 @@
+import clsx from "clsx";
 import { ReactNode } from "react";
 
 interface CommonLayoutProps {
   children: ReactNode;
   noPadding?: boolean;
-  className?: string;
   bottomFixedButton?: ReactNode;
   isFullScreen?: boolean;
+  className?: string;
 }
 
 export default function CommonLayout({
   children,
   noPadding = false,
-  className = "",
   bottomFixedButton,
   isFullScreen = false,
+  className = "",
 }: CommonLayoutProps) {
   return (
     <div
-      className={`${isFullScreen ? "relative h-screen overflow-hidden" : ""} ${
-        noPadding ? "" : "px-global"
-      } ${className}`}
+      className={clsx(
+        "flex flex-col w-full",
+        isFullScreen ? "relative h-full overflow-hidden" : "min-h-full",
+        !noPadding && "px-global",
+        className
+      )}
     >
       {children}
 
