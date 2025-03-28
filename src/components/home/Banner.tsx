6ac8fb38dfa985a89e4d947ad121bc3a6015e262
Kathleen Kang <kathleenkang@Kathleens-MacBook-Pro.local>
2025-03-28 22:13:32 +0900

    fix: make home banner image responsive

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -12,7 +12,7 @@ export default function Banner({ onClick, className }: BannerProps) {
     <button
       onClick={onClick}
       className={clsx(
-        "relative px-global cursor-pointer select-none rounded-[12px] overflow-hidden bg-white01",
+        "relative px-global py-1 cursor-pointer select-none rounded-[12px] bg-white01",
         className
       )}
     >
@@ -23,19 +23,22 @@ export default function Banner({ onClick, className }: BannerProps) {
           <p className="text-Caption1_R_12 text-[#CAB29A]">
             편지 보내기 전 읽어봐요
           </p>
-          <p className="text-Body1_B_16 ">
+          <p className="text-Body1_B_16">
             답답한 마음을 어떻게
             <br />
             풀어내야 할지 막막할 때는
           </p>
         </div>
 
-        <Image
-          src="/images/birds/home/home_birds.png"
-          alt="홈 새두마리"
-          width={126}
-          height={92}
-        />
+        <div className="max-w-[126px] flex-shrink">
+          <Image
+            src="/images/birds/home/home_birds.png"
+            alt="홈 새두마리"
+            width={126}
+            height={92}
+            className="w-full h-auto object-contain"
+          />
+        </div>
       </div>
     </button>
   );
