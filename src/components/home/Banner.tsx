c955a509318d0b09e78626d14fbadcd29bb77e3b
Kathleen Kang <kathleenkang@Kathleens-MacBook-Pro.local>
2025-03-28 22:06:52 +0900

    fix: banner background gradient

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -12,27 +12,31 @@ export default function Banner({ onClick, className }: BannerProps) {
     <button
       onClick={onClick}
       className={clsx(
-        "px-global cursor-pointer select-none flex justify-between items-center rounded-[12px] bg-[#FFD85BB2] bg-opacity-70 blur-[37px]",
+        "relative px-global cursor-pointer select-none rounded-[12px] overflow-hidden bg-white01",
         className
       )}
     >
-      <div>
-        <p className="text-Caption1_R_12 text-[#CAB29A]">
-          편지 보내기 전 읽어봐요
-        </p>
-        <p className="text-Body1_B_16 ">
-          답답한 마음을 어떻게
-          <br />
-          풀어내야 할지 막막할 때는
-        </p>
-      </div>
+      <div className="absolute inset-0 bg-radial-[at_50%_100%] from-[#fff5d4] to-70%" />
 
-      <Image
-        src="/images/birds/home/home_birds.png"
-        alt="홈 새두마리"
-        width={126}
-        height={92}
-      />
+      <div className="relative z-10 flex justify-between items-center">
+        <div className="text-left">
+          <p className="text-Caption1_R_12 text-[#CAB29A]">
+            편지 보내기 전 읽어봐요
+          </p>
+          <p className="text-Body1_B_16 ">
+            답답한 마음을 어떻게
+            <br />
+            풀어내야 할지 막막할 때는
+          </p>
+        </div>
+
+        <Image
+          src="/images/birds/home/home_birds.png"
+          alt="홈 새두마리"
+          width={126}
+          height={92}
+        />
+      </div>
     </button>
   );
 }
