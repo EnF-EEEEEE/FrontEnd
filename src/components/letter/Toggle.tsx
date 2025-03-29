a9b79d56674c348f9f1141ed38f52e5225c85e25
Kathleen Kang <kathleenkang@Kathleens-MacBook-Pro.local>
2025-03-29 20:23:59 +0900

    chore: refactor toggle component

--- src/components/letter/Toggle.tsx
+++ src/components/letter/Toggle.tsx
@@ -1,21 +1,24 @@
 "use client";
 
 import { useLetterStore } from "@/store/useLetterStore";
+import clsx from "clsx";
 
 export default function Toggle() {
   const { isNotificationOn, toggleNotification } = useLetterStore();
 
   return (
     <div
-      className={`w-[60px] h-[30px] rounded-[40px] border border-[#C7C7CC] ${
-        isNotificationOn ? "bg-[#292D32]" : "bg-[#C7C7CC]"
-      } flex items-center px-1 cursor-pointer transition-all duration-300`}
+      className={clsx(
+        "w-[60px] h-[30px] rounded-[40px] flex items-center px-1 cursor-pointer transition-all duration-300",
+        isNotificationOn ? "bg-black01" : "bg-gray03"
+      )}
       onClick={toggleNotification}
     >
       <div
-        className={`w-[26px] h-[26px] bg-white rounded-full shadow-md transition-transform duration-300 ${
-          isNotificationOn ? "translate-x-[25px]" : "translate-x-0"
-        }`}
+        className={clsx(
+          "w-[22px] h-[22px] bg-white02 rounded-full shadow-md transition-transform duration-300",
+          isNotificationOn ? "translate-x-[30px]" : "translate-x-0"
+        )}
       ></div>
     </div>
   );
